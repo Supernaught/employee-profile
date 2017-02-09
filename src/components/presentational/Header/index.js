ec3ef8f7393d1722ab103fed9d8d06dbd64a2590
davybolivar <davy.bolivar@gmail.com>
2017-02-09 15:44:48 +0800

    add header option dropdown

--- src/components/presentational/Header/index.js
+++ src/components/presentational/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { Sticky } from 'react-sticky';
 
 import SearchBox from '../../presentational/SearchBox';
+import ProfileAvatar from '../../presentational/ProfileAvatar';
 import { searchUser } from '../../../api/search-api';
 
 import './index.css';
@@ -39,7 +40,19 @@ export default class Header extends Component {
         	</div>
         	<div className="header__section">
             {searchboxComponent}
-        		Logout
+            <div className="header__nav dropdown">
+              <ProfileAvatar className="header__user-picture" />
+              <svg className="dropdown__toggle-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M7,10L12,15L17,10H7Z" />
+              </svg>
+              <div className="dropdown__menu">
+                <Link to="/employees/davy" className="dropdown__menu-item">Your Profile</Link>
+                <Link to="" className="dropdown__menu-item">Edit Profile</Link>
+                <Link to="/about" className="dropdown__menu-item">About</Link>
+                <hr/>
+                <Link to="" className="dropdown__menu-item">Sign out</Link>
+              </div>
+            </div>
         	</div>
         </div>
       </Sticky>
