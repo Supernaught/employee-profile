dd61dbd0cb6191a7e26aa129f19245e4f24168a1
davybolivar <davy.bolivar@gmail.com>
2017-02-11 22:55:57 +0800

    add banner to profile

--- src/components/presentational/UserPage/index.js
+++ src/components/presentational/UserPage/index.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import Header from '../../containers/HeaderContainer';
 
+import './index.css';
+
 export default class UserPage extends Component {
 	render() {
 		const userId = this.props.params.id;
 		return (
-			<div className="content page">
+			<div className="profile-page content page">
 				<Header />
+				<div className="profile-page__banner">
+
+				</div>
+				<div className="profile-page__nav-bar">
+				s
+				</div>
 				<div className="content__wrapper">
 					--{userId} Profile
 					{this.props.children}
