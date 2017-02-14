7b3ed1d9bc9283c4508401a241daa38274f2d843
davybolivar <davy.bolivar@gmail.com>
2017-02-14 10:55:06 +0800

    add header counter

--- src/components/presentational/UserPage/index.js
+++ src/components/presentational/UserPage/index.js
@@ -11,6 +11,28 @@ export default class UserPage extends Component {
 		return (
 			<div className="profile-page content page">
 				<Header />
+				<div className="banner banner--start-bottom content">
+					<div className="banner__content content__wrapper">
+						<div className="banner__counter-container">
+							<span className="banner__counter ">
+								<h4 className="banner__counter-label">VACATION LEAVES</h4>
+								<h2 data-denominator="/10" className="banner__counter-value banner__counter-value--fraction">10</h2>
+							</span>
+							<span className="banner__counter">
+								<h4 className="banner__counter-label">SICK LEAVES</h4>
+								<h2 data-denominator="/10" className="banner__counter-value banner__counter-value--fraction">8</h2>
+							</span>
+							<span className="banner__counter">
+								<h4 className="banner__counter-label">LATES THIS MONTH</h4>
+								<h2 className="banner__counter-value">0</h2>
+							</span>
+							<span className="banner__counter">
+								<h4 className="banner__counter-label">ABSENT</h4>
+								<h2 className="banner__counter-value">0</h2>
+							</span>
+						</div>
+					</div>
+				</div>
 				<div className="content__wrapper">
 					--{userId} Profile
 					{this.props.children}
