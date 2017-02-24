f0bb1bbfea59c2400204c0461deb9636aa675912
davybolivar <davy.bolivar@gmail.com>
2017-02-24 09:15:45 +0800

    add timeline sticky header

--- src/components/presentational/Timeline/index.js
+++ src/components/presentational/Timeline/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Sticky, StickyContainer } from 'react-sticky';
 
 import './index.css';
 
@@ -7,36 +8,124 @@ export default class Timeline extends Component {
 	
 	render() {
 		return (
+			<StickyContainer>
 			<ul className="timeline">
-				<h3 className="timeline__header">
-					<Link 
-						className="timeline__month-header"
-						to="/employees/davy?date=2017-2-1"
-						onClick={this.props.handleShowMonth.bind(this, '2017-2-1')}>
-						February
-						<span className="timeline__year-header">2018</span>
-					</Link>
-				</h3>
-				<li className="timeline__event timeline__event--late">Late</li>
-				<li className="timeline__event timeline__event--absent">Absent</li>
-				<li className="timeline__event timeline__event--leave">Leave</li>
-				<li className="timeline__event timeline__event--leave">Holiday</li>
-				<li className="timeline__event timeline__event--leave">Event</li>
-				<h3 className="timeline__header">
-					<Link 
-						className="timeline__month-header"
-						to="/employees/davy?date=2017-1-1"
-						onClick={this.props.handleShowMonth.bind(this, '2017-1-1')}>
-						January
-						<span className="timeline__year-header">2018</span>
-					</Link>
-				</h3>
-				<li className="timeline__event timeline__event--late">Late</li>
-				<li className="timeline__event timeline__event--absent">Absent</li>
-				<li className="timeline__event timeline__event--leave">Leave</li>
-				<li className="timeline__event timeline__event--leave">Holiday</li>
-				<li className="timeline__event timeline__event--leave">Event</li>
+				<div className="timeline__sticky-container">
+					<Sticky className="timeline__sticky-header">
+						<h3 className="timeline__header">
+							<Link 
+								className="timeline__month-header"
+								to="/employees/davy?date=2017-2-1"
+								onClick={this.props.handleShowMonth.bind(this, '2017-2-1')}>
+								February
+								<span className="timeline__year-header">2018</span>
+							</Link>
+						</h3>
+					</Sticky>
+				</div>
+				<li className="timeline__item-container">
+					<div className="event event--late">
+						Late
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--absent">
+						Absent
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--leave">
+						Leave
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--holiday">
+						Holiday
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--event">
+						Event
+					</div>
+				</li>
+				<div className="timeline__sticky-container">
+					<Sticky className="timeline__sticky-header">
+						<h3 className="timeline__header">
+							<Link 
+								className="timeline__month-header"
+								to="/employees/davy?date=2017-1-1"
+								onClick={this.props.handleShowMonth.bind(this, '2017-1-1')}>
+								January
+								<span className="timeline__year-header">2018</span>
+							</Link>
+						</h3>
+					</Sticky>
+				</div>
+				<li className="timeline__item-container">
+					<div className="event event--late">
+						Late
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--absent">
+						Absent
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--leave">
+						Leave
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--holiday">
+						Holiday
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--event">
+						Event
+					</div>
+				</li>
+				<div className="timeline__sticky-container">
+					<Sticky className="timeline__sticky-header">
+						<h3 className="timeline__header">
+							<Link 
+								className="timeline__month-header"
+								to="/employees/davy?date=2016-12-1"
+								onClick={this.props.handleShowMonth.bind(this, '2016-12-1')}>
+								December
+								<span className="timeline__year-header">2017</span>
+							</Link>
+						</h3>
+					</Sticky>
+				</div>
+				<li className="timeline__item-container">
+					<div className="event event--late">
+						Late
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--absent">
+						Absent
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--leave">
+						Leave
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--holiday">
+						Holiday
+					</div>
+				</li>
+				<li className="timeline__item-container">
+					<div className="event event--event">
+						Event
+					</div>
+				</li>
 			</ul>
+			</StickyContainer>
 		)
 	}
 }
